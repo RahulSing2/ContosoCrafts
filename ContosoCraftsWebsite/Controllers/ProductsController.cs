51c9af4a907548e64bdb258857525131b6400761
RahulSing2 <88368239+RahulSing2@users.noreply.github.com>
2021-08-11 13:25:50 +0530

    ContosoCrafts

---
+++ ContosoCraftsWebsite/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using ContosoCraftsWebsite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCraftsWebsite.Models;
+
+namespace ContosoCraftsWebsite.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+        public JsonFileProductService ProductService { get; }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+        //[HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery]string productId,
+            [FromQuery]int Rating)
+        {
+            ProductService.AddRating(productId, Rating);
+                return Ok();
+        }
+    }
+}
